e7a2b8dcd17365b8c0722cdfb6514bece6ef6a1f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-02-18 21:34:52 +0100

    [Studio] make pimcore bundles optional - only hard requirement on core

--- AdminClass/EventListener/PimcoreAdminListener.php
+++ AdminClass/EventListener/PimcoreAdminListener.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  *
  */
 
-namespace CoreShop\Bundle\MenuBundle\EventListener;
+namespace CoreShop\Bundle\MenuBundle\AdminClass\EventListener;
 
 use Pimcore\Bundle\AdminBundle\Event\BundleManagerEvents;
 use Pimcore\Event\BundleManager\PathsEvent;
